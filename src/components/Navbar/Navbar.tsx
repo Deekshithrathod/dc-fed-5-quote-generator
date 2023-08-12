c5219b58347a7aed13d075a172ca0a7d80784837
deekshith <deekshith@Mac-mini.local>
2023-08-12 09:22:28 +0530

    added reverse navigation from author quotes page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,10 +3,14 @@ import "./Navbar.css";
 import { quoteState } from "../../atoms/quote";
 import { IQuote } from "../Quote/Quote";
 import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const BASE_URL = `https://quote-garden.onrender.com/api/v3/quotes/`;
 
 const Navbar = () => {
+  const { authorName } = useParams();
+  const navigate = useNavigate();
+
   const [isLoading, setIsLoading] = useState(false);
   const setQuote = useSetRecoilState(quoteState);
 
@@ -22,6 +26,10 @@ const Navbar = () => {
     fetch(`${BASE_URL}/random`)
       .then((res) => res.json())
       .then((data) => updateQuote(data.data[0]));
+
+    if (authorName) {
+      navigate("/");
+    }
   };
 
   return (
